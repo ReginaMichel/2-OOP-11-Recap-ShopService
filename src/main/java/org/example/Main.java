e21ce1c6fefdf254f9b57b0b6eb64992524050a3
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-09 11:41:14 +0200

    Added Methods to get all stored Products of a ProductRepo, get the amount of a certain Product in the Repo, remove 1 element of a certain Product in a Repo, remove all of a Product type, and remove all Products in a repo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Product zahnpasta = new Product("Zahnpasta",
+                "Reinigungsmittel für Zähne",
+                new BigDecimal("1.99"));
+        ProductRepo store = new ProductRepo();
+        store.addProduct(zahnpasta);
+        store.addProduct(zahnpasta);
+        store.addProduct(zahnpasta);
+        store.addProduct(zahnpasta);
+        store.addProduct(zahnpasta);
+        store.addProduct(zahnpasta);
+        System.out.println(store.getStoredProducts());
+        System.out.println(store.amountOfSpecificProduct(zahnpasta));
+        store.removeProductType(zahnpasta);
+        System.out.println(store.getStoredProducts());
     }
 }
--- src/main/java/org/example/ProductRepo.java
+++ src/main/java/org/example/ProductRepo.java
@@ -11,4 +11,33 @@ public class ProductRepo {
     public ProductRepo(ArrayList<Product> storedProducts) {
         this.storedProducts = storedProducts;
     }
+
+    public ArrayList<Product> getStoredProducts() {
+        return storedProducts;
+    }
+
+    public void addProduct(Product newProduct) {
+        this.storedProducts.add(newProduct);
+    }
+    public void removeSingleProduct(Product product) {
+        this.storedProducts.remove(product);
+    }
+    public int amountOfSpecificProduct(Product product) {
+        int count = 0;
+        for (Product p : this.storedProducts) {
+            if (p.equals(product)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public void removeProductType(Product product) {
+        int count = amountOfSpecificProduct(product);
+        for (int i = 0; i < count; i++) {
+            removeSingleProduct(product);
+        }
+    }
+    public void removeAllProducts() {
+        this.storedProducts.clear();
+    }
 }
